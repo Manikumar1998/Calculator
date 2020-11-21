f08f755138132d59fed57cf852c4ed4d5d553934
Manikumar Perla <manikumarperla18@gmail.com>
2020-11-21 23:32:07 +0900

    Implemented square() function

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a - b
+
 
 def mul(a, b):
     return a * b
 
+
 def div(a, b):
     return a / b
+
+
+def square(a):
+    return a ** 2
+
 
 if __name__ == "__main__":
     print(add(1, 2))
     print(sub(1, 2))
     print(mul(1, 2))
     print(div(1, 2))
+    print(square(2))
