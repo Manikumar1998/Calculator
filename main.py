c28d2999ce565721439bd0a7aee78a80b0c541b7
Manikumar Perla <manikumarperla18@gmail.com>
2020-11-21 23:24:46 +0900

    Implemented div() function

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def sub(a, b):
 def mul(a, b):
     return a * b
 
+def div(a, b):
+    return a / b
+
 if __name__ == "__main__":
     print(add(1, 2))
     print(sub(1, 2))
     print(mul(1, 2))
+    print(div(1, 2))
